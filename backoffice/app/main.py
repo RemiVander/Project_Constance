d7622f6115ceac8932266aafc2fb8c45bebf23e1
RemiVander <rm.vanderperre@gmail.com>
2025-11-24 11:26:37 +0100

    clean docstrings

--- backoffice/app/main.py
+++ backoffice/app/main.py
@@ -9,18 +9,16 @@ from . import boutique_api
 
 app = FastAPI(title="Back-office B2B Robes v4.1")
 
-# Middleware sessions admin
 app.add_middleware(SessionMiddleware, secret_key="CHANGE_ME_SECRET_KEY")
 
-# ⚠ Important pour le futur front (Next.js)
 origins = [
-    "http://localhost:3000",   # front en dev
+    "http://localhost:3000",
     # "https://front-tondomaine.com",  # à activer en prod
 ]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,      # indispensable pour les cookies
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
